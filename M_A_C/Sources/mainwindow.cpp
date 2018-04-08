0b433881b4a134fd38f84038c50066608ac3c931
4k1x <f.c.v.1996@gmail.com>
2018-04-08 21:10:57 +0200

    Cambiar icono

--- M_A_C/Sources/mainwindow.cpp
+++ M_A_C/Sources/mainwindow.cpp
@@ -1,5 +1,6 @@
-#include "mainwindow.h"
+#include "Headers/mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QWidget"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,13 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+
+void MainWindow::on_play_puaseButton_clicked()
+{
+    QPixmap pixmap(":/new/iconos/Iconos/icono-pause.png");
+    QIcon icono_pausa(pixmap);
+    ui->play_puaseButton->setIcon(icono_pausa);
 }
